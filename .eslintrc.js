c4a0732bb11339ceeda8cfa6895d4e2b65be8e87
Luc THQ <luc.ta@cptech.vn>
2022-10-12 13:26:02 +0700

    up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,27 @@
+const prettier = require('./prettier');
+
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'eslint-plugin-import-helpers'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
   ],
   root: true,
   env: {
     node: true,
     jest: true,
   },
+
   ignorePatterns: ['.eslintrc.js'],
+
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
